2c2145566aec8088145a7246a04562fd280239ef
Dave Arneson <urvaius@gmail.com>
2016-10-18 16:20:48 -0700

    working on not doing sound yet tghough

--- Halloween4/Resources/Resource.Designer.cs
+++ Halloween4/Resources/Resource.Designer.cs
@@ -45,7 +45,10 @@ namespace Halloween4
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int halloween3 = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
 			
 			static Drawable()
 			{
@@ -60,14 +63,14 @@ namespace Halloween4
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int myListView = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int myListView = 2131099650;
 			
-			// aapt resource value: 0x7f050001
-			public const int txtDuration = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int txtDuration = 2131099649;
 			
-			// aapt resource value: 0x7f050000
-			public const int txtName = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int txtName = 2131099648;
 			
 			static Id()
 			{
@@ -98,14 +101,33 @@ namespace Halloween4
 			}
 		}
 		
+		public partial class Raw
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int Buffysound = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int eye = 2130968577;
+			
+			static Raw()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Raw()
+			{
+			}
+		}
+		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int ApplicationName = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int Hello = 2131034112;
 			
 			static String()
 			{
---
+++ Halloween4/SoundManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Media;
+
+namespace Halloween4
+{
+	public class SoundManager
+
+	{
+		MediaPlayer player;
+		String Name;
+		void PlaySound(String name)
+		{
+			this.Name = name;
+			player = MediaPlayer.Create(this, Resource.Raw.eye);
+
+
+
+
+		}
+
+
+	}
+}
